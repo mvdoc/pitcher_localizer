58e159a2d014fcaffd79f7cdcbb696ba8020d00c
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2017-11-03 17:50:02 -0400

    Enable CL arguments to run localizer
    And automatically create stimulus order for new subject ids

--- run_localizer.py
+++ run_localizer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""
+Presentation script for a face/object/scene/bodies localizer, inspired by
+the paradigm in Pitcher, D., Dilks, D. D., Saxe, R. R., Triantafyllou, C.,
+& Kanwisher, N. (2011). Differential selectivity for dynamic versus static
+information in face-selective cortical regions. Neuroimage, 56(4), 2356-2363.
+"""
+import argparse
 import sys
 from psychopy import visual, gui, event, logging
 from psychopy import sound, core
@@ -10,12 +17,21 @@ import json
 import csv
 import datetime
 import shutil
+import subprocess as sp
 
 # add a new level name called bids
 # we will use this level to log information that will be saved
 # in the _events.tsv file for this run
 BIDS = 26
 logging.addLevel(BIDS, 'BIDS')
+
+
+def move_halted_log(fn):
+    # flush log
+    logging.flush()
+    shutil.move(fn, fn.replace('.txt', '__halted.txt'))
+    # quit
+    core.quit()
 
 
 def logbids(msg, t=None, obj=None):
@@ -72,181 +88,205 @@ instructions = config['instructions']
 subjectlog = pjoin(RESDIR, config['log_subjects'])
 lastinfo = load_subjectlog(subjectlog)
 
-# ask info to experimenter
-info = {
-    'subject_id': lastinfo['subject_id'],
-    'run_nr': 1,
-    'scanner?': True,
-    'fullscr': True,
-}
-infdlg = gui.DlgFromDict(dictionary=info,
-                         title="Movie Presentation",
-                         order=['subject_id', 'run_nr', 'scanner?', 'fullscr']
-                         )
-if not infdlg.OK:
-    core.quit()
-# save log of subjects
-write_subjectlog(subjectlog, info)
 
-run_nr = int(info['run_nr'])
-subj = info['subject_id']
-fullscr = info['fullscr']
-
-time = core.Clock()
-RESDIR = pjoin(RESDIR, subj)
-if not pexists(RESDIR):
-    os.makedirs(RESDIR)
-
-log_fn = config['log_template'].format(
-    subj=subj,
-    task_name=config['task_name'],
-    runnr=run_nr,
-    timestamp=ptime.strftime(time_template),
-)
-log_fn = pjoin(RESDIR, log_fn)
-log_responses = logging.LogFile(log_fn, level=logging.INFO)
-
-
-def move_halted_log(fn):
-    # flush log
-    logging.flush()
-    shutil.move(fn, fn.replace('.txt', '__halted.txt'))
-    # quit
-    core.quit()
-
-# set up global key for quitting; if that happens, log will be moved to
-# {log_fn}__halted.txt
-event.globalKeys.add(key='q',
-                     modifiers=['ctrl'],
-                     func=move_halted_log,
-                     func_args=[log_fn],
-                     name='quit experiment gracefully')
-
-# --- LOAD STIMULI ORDER FOR THIS PARTICIPANT ---
-stim_json = pjoin('cfg', 'sub-{0}_task-localizer_4runs.json'.format(subj))
-with open(stim_json, 'rb') as f:
-    stimuli = json.load(f)[str(run_nr)]
-# ------------------------
-
-print "Opening screen"
-tbegin = time.getTime()
-using_scanner = info['scanner?']
-# Setting up visual
-if using_scanner:
-    size = [1280, 1024]
-    fullscr = fullscr
-else:
-    size = [1024, 768]
-    fullscr = fullscr
-scrwin = visual.Window(size=size,
-                       allowGUI=False, units='pix',
-                       screen=1, rgb=[-1, -1, -1],
-                       fullscr=fullscr)
-# load clips
-print "Loading stimuli"
-loading = visual.TextStim(scrwin,
-                          text="Loading stimuli...",
-                          height=31)
-loading.draw()
-scrwin.flip()
-stimuli_clip = dict()
-for stim in stimuli:
-    if stim['stim_type'] != 'fixation':
-        stim_fn = stim['stim_fn']
-        print("Loading {0}".format(stim_fn))
-        stimuli_clip[stim_fn] = \
-            visual.MovieStim3(scrwin, stim_fn, size=1280, name=stim_fn,
-                              noAudio=True, loop=True)
-
-scrwin.flip()
-cross_hair = visual.TextStim(scrwin, text='+', height=31,
-                             pos=(0, 0), color='#FFFFFF')
-
-
-if using_scanner:
-    intro_msg = "Waiting for trigger..."
-else:
-    intro_msg = "Press Enter to start"
-intro_msg = instructions + '\n' + intro_msg
-intro = visual.TextStim(scrwin, text=intro_msg, height=31)
-
-# Start of experiment
-intro.draw()
-scrwin.flip()
-# open up serial port and wait for first trigger
-if using_scanner:
-    ser_port = '/dev/ttyUSB0'
-    ser = serial.Serial(ser_port, 115200, timeout=.0001)
-    ser.flushInput()
-    trigger = ''
-    while trigger != '5':
-        trigger = ser.read()
-else:
-    from psychopy.hardware.emulator import launchScan
-    event.waitKeys(keyList=['return'])
-    # XXX: set up TR here
-    MR_settings = {
-        'TR': 1,
-        'volumes': 280,
-        'sync': '5',
-        'skip': 3,
-        'sound': False,
-    }
-    vol = launchScan(scrwin, MR_settings, globalClock=time, mode='Test')
-
-    class FakeSerial(object):
-        @staticmethod
-        def read():
-            k = event.getKeys(['1', '2', '5'])
-            return k[-1] if k else ''
-    ser = FakeSerial()
-
-# set up timer for experiment starting from first trigger
-timer_exp = core.Clock()
-trunbegin = timer_exp.getTime()
-# setup bids log
-logbids("onset\tduration\tstim_type\trepetition")
-# duration will be filled later
-template_bids = '{onset:.3f}\t{duration:.3f}\t{stim_type}\t{stim_fn}\t' \
-                '{repetition}'
-# and now we just loop through the trials
-for trial in stimuli:
-    stim_type = trial['stim_type']
-    stim_fn = trial['stim_fn']
-    duration = trial['duration']
-    logbids(template_bids.format(
-        onset=timer_exp.getTime(),
-        duration=duration,
-        stim_type=stim_type,
-        stim_fn=stim_fn,
-        repetition=trial.get('repetition', 0)),
+def main(info):
+    # save log of subjects
+    write_subjectlog(subjectlog, info)
+    run_nr = int(info['run_nr'])
+    subj = info['subject_id']
+    fullscr = info['fullscr']
+    time = core.Clock()
+    subj_dir = pjoin(RESDIR, subj)
+    if not pexists(subj_dir):
+        os.makedirs(subj_dir)
+    log_fn = config['log_template'].format(
+        subj=subj,
+        task_name=config['task_name'],
+        runnr=run_nr,
+        timestamp=ptime.strftime(time_template),
     )
-    trial_counter = core.CountdownTimer(duration)
-    if stim_type == 'fixation':
-        cross_hair.draw()
-        scrwin.flip()
-        logging.flush()
-        while trial_counter.getTime() > 0:
-            pass
+    log_fn = pjoin(subj_dir, log_fn)
+    log_responses = logging.LogFile(log_fn, level=logging.INFO)
+    # set up global key for quitting; if that happens, log will be moved to
+    # {log_fn}__halted.txt
+    event.globalKeys.add(key='q',
+                         modifiers=['ctrl'],
+                         func=move_halted_log,
+                         func_args=[log_fn],
+                         name='quit experiment gracefully')
+    # --- LOAD STIMULI ORDER FOR THIS PARTICIPANT ---
+    stim_json = pjoin('cfg', 'sub-{0}_task-localizer_4runs.json'.format(subj))
+    # create stimulus order if not existing
+    if not os.path.exists(stim_json):
+        logging.warning("Creating stimulus order for {0}".format(subj))
+        cmd = "python make_stim_order.py --subid {subj} --output {output} " \
+              "--nruns 4".format(subj=subj, output=os.path.dirname(stim_json))
+        logging.warning("Running '{0}'".format(cmd))
+        sp.check_call(cmd.split())
+    with open(stim_json, 'rb') as f:
+        stimuli = json.load(f)[str(run_nr)]
+    # ------------------------
+    print "Opening screen"
+    tbegin = time.getTime()
+    using_scanner = info['scanner?']
+    # Setting up visual
+    if using_scanner:
+        size = [1280, 1024]
+        fullscr = fullscr
     else:
-        movie = stimuli_clip[stim_fn]
-        while trial_counter.getTime() > 0:
-            key = ser.read()
-            if key in ['1', '2']:
-                logbids(template_bids.format(
-                    onset=timer_exp.getTime(),
-                    duration=0.,
-                    stim_type='button_press',
-                    stim_fn=None,
-                    repetition=0)
-                )
-            if movie.status != visual.FINISHED:
-                movie.draw()
-                scrwin.flip()
-            else:
-                cross_hair.draw()
-                scrwin.flip()
-logging.exp("Done in {0:.2f}s".format(timer_exp.getTime()))
-logging.flush()
-scrwin.close()
-core.quit()
+        size = [1024, 768]
+        fullscr = fullscr
+    scrwin = visual.Window(size=size,
+                           allowGUI=False, units='pix',
+                           screen=1, rgb=[-1, -1, -1],
+                           fullscr=fullscr)
+    # load clips
+    print "Loading stimuli"
+    loading = visual.TextStim(scrwin,
+                              text="Loading stimuli...",
+                              height=31)
+    loading.draw()
+    scrwin.flip()
+    stimuli_clip = dict()
+    for stim in stimuli:
+        if stim['stim_type'] != 'fixation':
+            stim_fn = stim['stim_fn']
+            print("Loading {0}".format(stim_fn))
+            stimuli_clip[stim_fn] = \
+                visual.MovieStim3(scrwin, stim_fn, size=(1280, 940),
+                                  name=stim_fn,
+                                  noAudio=True, loop=True)
+    scrwin.flip()
+    cross_hair = visual.TextStim(scrwin, text='+', height=31,
+                                 pos=(0, 0), color='#FFFFFF')
+    if using_scanner:
+        intro_msg = "Waiting for trigger..."
+    else:
+        intro_msg = "Press Enter to start"
+    intro_msg = instructions + '\n' + intro_msg
+    intro = visual.TextStim(scrwin, text=intro_msg, height=31)
+    # Start of experiment
+    intro.draw()
+    scrwin.flip()
+    # open up serial port and wait for first trigger
+    if using_scanner:
+        ser_port = '/dev/ttyUSB0'
+        ser = serial.Serial(ser_port, 115200, timeout=.0001)
+        ser.flushInput()
+        trigger = ''
+        while trigger != '5':
+            trigger = ser.read()
+    else:
+        from psychopy.hardware.emulator import launchScan
+        event.waitKeys(keyList=['return'])
+        # XXX: set up TR here
+        MR_settings = {
+            'TR': 1,
+            'volumes': 280,
+            'sync': '5',
+            'skip': 3,
+            'sound': False,
+        }
+        vol = launchScan(scrwin, MR_settings, globalClock=time, mode='Test')
+
+        class FakeSerial(object):
+            @staticmethod
+            def read():
+                k = event.getKeys(['1', '2', '5'])
+                return k[-1] if k else ''
+
+        ser = FakeSerial()
+
+    # set up timer for experiment starting from first trigger
+    timer_exp = core.Clock()
+    trunbegin = timer_exp.getTime()
+    # setup bids log
+    logbids("onset\tduration\tstim_type\trepetition")
+    # duration will be filled later
+    template_bids = '{onset:.3f}\t{duration:.3f}\t{stim_type}\t{stim_fn}\t' \
+                    '{repetition}'
+    # and now we just loop through the trials
+    for trial in stimuli:
+        stim_type = trial['stim_type']
+        stim_fn = trial['stim_fn']
+        duration = trial['duration']
+        logbids(template_bids.format(
+            onset=timer_exp.getTime(),
+            duration=duration,
+            stim_type=stim_type,
+            stim_fn=stim_fn,
+            repetition=trial.get('repetition', 0)),
+        )
+        trial_counter = core.CountdownTimer(duration)
+        if stim_type == 'fixation':
+            cross_hair.draw()
+            scrwin.flip()
+            logging.flush()
+            while trial_counter.getTime() > 0:
+                pass
+        else:
+            movie = stimuli_clip[stim_fn]
+            while trial_counter.getTime() > 0:
+                key = ser.read()
+                if key in ['1', '2']:
+                    logbids(template_bids.format(
+                        onset=timer_exp.getTime(),
+                        duration=0.,
+                        stim_type='button_press',
+                        stim_fn=None,
+                        repetition=0)
+                    )
+                if movie.status != visual.FINISHED:
+                    movie.draw()
+                    scrwin.flip()
+                else:
+                    cross_hair.draw()
+                    scrwin.flip()
+    logging.exp("Done in {0:.2f}s".format(timer_exp.getTime()))
+    logging.flush()
+    scrwin.close()
+    core.quit()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('--subject', '-s', type=str,
+                        help='subject id')
+    parser.add_argument('--runnr', '-r', type=int,
+                        help='run nr',
+                        choices=range(1, 5))
+    parser.add_argument('--no-scanner', action='store_false',
+                        help='do not listen to the serial port')
+    parser.add_argument('--no-fullscreen', action='store_false',
+                        help='do not run in fullscreen')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    parsed = parse_args()
+    # ask info to experimenter if no args passed
+
+    if parsed.subject is None:
+        info = {
+            'subject_id': lastinfo['subject_id'],
+            'run_nr': 1,
+            'scanner?': True,
+            'fullscr': True,
+        }
+        infdlg = gui.DlgFromDict(dictionary=info,
+                                 title="Movie Presentation",
+                                 order=['subject_id', 'run_nr', 'scanner?',
+                                        'fullscr']
+                                 )
+        if not infdlg.OK:
+            core.quit()
+    else:
+        info = {
+            'subject_id': parsed.subject,
+            'run_nr': parsed.runnr,
+            'scanner?': parsed.no_scanner,
+            'fullscr': parsed.no_fullscreen,
+        }
+
+    main(info)
